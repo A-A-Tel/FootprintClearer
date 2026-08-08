c84fbd5f79ca7b8bb237089b636026ee4c1b4093
a-a-tel <anouaranthonytel@gmail.com>
2026-08-09 00:34:01 +0200

    added private mode for logins

--- FootprintClearer/Views/Discord/DiscordLoginView.axaml.cs
+++ FootprintClearer/Views/Discord/DiscordLoginView.axaml.cs
@@ -1,5 +1,6 @@
 using System;
 using Avalonia.Controls;
+using Avalonia.Platform;
 using FootprintClearer.ViewModels.Discord;
 
 namespace FootprintClearer.Views.Discord;
@@ -13,7 +14,7 @@ public partial class DiscordLoginView : UserControl
         InitializeComponent();
     }
 
-    private async void NativeWebView_OnNavigationStarted(object? sender, WebViewNavigationStartingEventArgs webViewNavigationStartingEventArgs)
+    private async void WebView_OnNavigationStarted(object? sender, WebViewNavigationStartingEventArgs webViewNavigationStartingEventArgs)
     {
         await ViewModel.HandlePageLoad(WebView);
     }
@@ -21,5 +22,27 @@ public partial class DiscordLoginView : UserControl
     private void WebView_OnWebMessageReceived(object? sender, WebMessageReceivedEventArgs e)
     {
         Console.WriteLine($"{DateTime.Now} - INCOMING MESSAGE: {e.Body}");
+    }
+
+    private void WebView_OnEnvironmentRequested(object? sender, WebViewEnvironmentRequestedEventArgs e)
+    {
+        e.EnableDevTools = true;
+
+        switch (e)
+        {
+            case WindowsWebView2EnvironmentRequestedEventArgs args:
+                args.IsInPrivateModeEnabled = true;
+                break;
+            case AppleWKWebViewEnvironmentRequestedEventArgs args:
+                args.NonPersistentDataStore = true;
+                break;
+            case LinuxWpeWebViewEnvironmentRequestedEventArgs wpeArgs:
+                wpeArgs.DataDirectory = null;
+                wpeArgs.CacheDirectory = null;
+                break;
+            case GtkWebViewEnvironmentRequestedEventArgs gtkArgs:
+                gtkArgs.EphemeralDataManager = true;
+                break;
+        }
     }
 }
